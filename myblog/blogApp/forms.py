cba73df592e5dba0a15e91122f84379e96141768
l0iso <akg.postbox@gmail.com>
2016-07-12 18:26:19 +0300

    making API

---
+++ myblog/blogApp/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from .models import Comments, BlogPost
+
+class CommentsForm (forms.ModelForm):
+    class Meta:
+        model = Comments
+        fields = ('comment_author','comment_text',)
+
+class PostForm (forms.ModelForm):
+
+    class Meta:
+        model = BlogPost
+        fields = ('post_head','post_author','post_text',)
+        widgets = {"post_text": forms.widgets.Textarea(attrs={'rows': 2})}
